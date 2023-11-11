fb6c72ecda58bbff9f356f10de86f78791bd38da
superOldman <15221955725@163.com>
2023-11-12 00:12:33 +0800

    修改路由图标

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -6,10 +6,10 @@
         v-if="!item.meta.hidden"
         @click="goRoute"
       >
-        <template #title>
           <el-icon>
             <component :is="item.meta.icon"></component>
           </el-icon>
+        <template #title>
           <span>{{ item.meta.title }}</span>
         </template>
       </el-menu-item>
@@ -21,10 +21,10 @@
         v-if="!item.children[0].meta.hidden"
         @click="goRoute"
       >
+        <el-icon>
+          <component :is="item.children[0].meta.icon"></component>
+        </el-icon>
         <template #title>
-          <el-icon>
-            <component :is="item.children[0].meta.icon"></component>
-          </el-icon>
           <span>{{ item.children[0].meta.title }}</span>
         </template>
       </el-menu-item>
